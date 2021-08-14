4c92b613bf9c616e085ba7ae624035394f35eb0c
YiroYi <yiroyi@live.com>
2021-08-14 21:45:56 +0900

    Adding query to get token

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require('jsonwebtoken');
 
+const SECRET = process.env.SECRET;
 module.exports = {
   createUser: async function ({ userInput }, req) {
     const errors = [];
@@ -42,4 +44,34 @@ module.exports = {
 
     return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
+  login: async function({ email, password }) {
+    const user = await User.findOne({email: email});
+
+    if(!user) {
+      const error = new Error('User not found');
+      error.code = 401;
+      throw error;
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password);
+
+    if(!isEqual) {
+      const error = new Error('Incorrect password');
+      error.code = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({
+      userId: user._id.toString(),
+      email: user.email
+    }, SECRET, { expiresIn: '1h'});
+
+    return { token: token, userId: user._id.toString() };
+  }
 };
+
+
+
+
+
+
